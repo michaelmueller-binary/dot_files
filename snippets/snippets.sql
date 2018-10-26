261dbb000ef0c41a9026f8efc8a68369565f9db9
Michael Mueller <michael.mueller@binary.com>
2018-10-26 12:54:14 +1100

    Update snippets.sql

--- snippets/snippets.sql
+++ snippets/snippets.sql
@@ -1,16 +1,7 @@
 snippet fun
-	CREATE OR REPLACE FUNCTION betonmarkets.delete_copiers(
-		p_trader_id VARCHAR(12),
-		p_copier_id VARCHAR(12),
-		p_token TEXT
+	CREATE OR REPLACE FUNCTION ${1}(
+		p_var VARCHAR(12),
 	) RETURNS BIGINT  AS $$
-	WITH del AS (
-	DELETE FROM betonmarkets.copiers 
-	WHERE trader_id = p_trader_id 
-	AND copier_id = p_copier_id 
-	AND ((p_token IS NULL AND trader_token IS NULL ) 
-		OR  trader_token =  p_token)
-	RETURNING trader_id
-	)
-	SELECT count(trader_id) FROM del;
+	BEGIN
+	END
 	$$ LANGUAGE SQL;
